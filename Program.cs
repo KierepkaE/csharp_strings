3ee7fe981e665f1bddb5eda46eb3257e52e22e9f
kierepkae <kierepkaedyta@gmail.com>
2019-11-13 14:26:52 +0000

    use Join method on the array

--- Program.cs
+++ Program.cs
@@ -3,8 +3,14 @@ using System;
 namespace StringPractice {
   class Program {
     static void Main (string[] args) {
-      string firstName = "Joe";
-      string lastName = "Black";
+      var firstName = "Joe";
+      var lastName = "Black";
+
+      var fullName = firstName + " " + lastName;
+      var fullName2 = string.Format ("My name is {0} {1}", firstName, lastName);
+      var names = new string[3] { "John", "Jack", "Jo" };
+      var formattedNames = string.Join (",", names);
+      Console.WriteLine (formattedNames);
     }
 
   }
