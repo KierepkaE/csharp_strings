357441e84373431ddce98117b68f510f17fec318
kierepkae <kierepkaedyta@gmail.com>
2019-11-13 14:21:17 +0000

    defined firstName and lastNAme

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using System;
 namespace StringPractice {
   class Program {
     static void Main (string[] args) {
-
+      string firstName = "Joe";
+      string lastName = "Black";
     }
 
   }
